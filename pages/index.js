9230045c4880b3e0b9c16b3030df5aacc94273f4
jaymesC <clementjaymes@gmail.com>
2021-11-10 08:49:54 -0800

    updated project files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,9 +8,20 @@ export default function Home() {
         <title>Slider Page</title>
         <meta name="slider" content="" />
       </Head>
-      <h1>index page</h1>
-      <h1>index page</h1>
-      <h1>index page</h1>
+      <div className="flex mt-5 text-lg justify-center  lg:mt-20 lg:text-4xl font-medium">
+        <h1 className="text-red-400 mr-4 ">/</h1>
+        <h1 className="text-center text-gray-700">Reviews</h1>
+      </div>
+      <div className="flex justify-center mt-16">
+        <Image
+          src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
+          alt="pic"
+          width={150}
+          height={150}
+          layout="fixed"
+          className="rounded-full border-solid border-8 border-gray-800"
+        />
+      </div>
     </div>
-  )
+  );
 }
