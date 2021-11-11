8b93f0b700d60ea1befce476a6a76a42851fb917
jaymesC <clementjaymes@gmail.com>
2021-11-11 09:58:22 -0800

    updated project files

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
         <meta name="slider" content="" />
       </Head>
       <div className="flex mt-5 text-lg justify-center  lg:mt-20 lg:text-4xl font-medium">
-        <h1 className="text-red-400 mr-4 ">/</h1>
+        <h1 className="text-yellow-700 mr-4 ">/</h1>
         <h1 className="text-center text-gray-700">Reviews</h1>
       </div>
       <div className="flex justify-center mt-16 ">
-        <ChevronLeftIcon className="h-8 cursor-pointer bg-gray-600 rounded-md mr-3 self-center mt-16" />
+        <ChevronLeftIcon className="h-8 cursor-pointer bg-gray-400 rounded-md mr-3 self-center mt-16" />
         <div className="flex flex-col justify-center items-center">
           <Image
             src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
@@ -25,7 +25,9 @@ export default function Home() {
             className="border-solid object-cover  rounded-full  border-8 border-gray-800"
           />
 
-          <h1 className="text-center mt-5">Marie Ferguson</h1>
+          <h1 className="text-center text-lg mt-5 text-yellow-700">
+            Marie Ferguson
+          </h1>
           <h4 className="text-center ">Office Manager</h4>
           <p className="text-center mt-5">
             Fingerstache umami squid, kinfolk subway tile selvage tumblr man
@@ -35,7 +37,7 @@ export default function Home() {
             fanny pack raclette pok pok mustache.
           </p>
         </div>
-        <ChevronRightIcon className="h-8 cursor-pointer bg-gray-600 rounded-md ml-3 self-center mt-16" />
+        <ChevronRightIcon className="h-8 cursor-pointer bg-gray-400 rounded-md ml-3 self-center mt-16" />
       </div>
     </div>
   );
