cc17d411befcbc531b1ca5c5286b39b17caf1774
jaymesC <clementjaymes@gmail.com>
2021-11-12 07:32:38 -0800

    updated data file

---
+++ data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    name: "MARIA FERGURSEN",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    position: "Office Manager",
+    description:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    name: "JOHN DOE",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    position: "Regular Guy",
+    description:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+  },
+  {
+    name: "PETER SMITH",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    position: "Product Designer",
+    description:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    name: "SUSAN ANDERSEN",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    position: "THE BOSS",
+    description:
+      "ymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag.",
+  },
+];
