9a0be2413d202a1f054b16bd314124d438522b0a
jaymesC <clementjaymes@gmail.com>
2021-11-13 08:14:56 -0800

    updated project files

--- pages/index.js
+++ pages/index.js
@@ -19,38 +19,36 @@ export default function Home() {
       </div>
       <div className="flex justify-center mt-16 ">
         <ChevronLeftIcon className="h-8 cursor-pointer bg-gray-400 rounded-md mr-3 self-center mt-16" />
-          <div
-            key={data[people].id}
-            className="flex flex-col justify-center items-center text-gray-500 "
+        <div
+          key={data[people].id}
+          className="flex flex-col justify-center items-center text-gray-500 w-1/2"
+        >
+          <Image
+            src={data[people].image}
+            alt="pic"
+            width={150}
+            height={150}
+            layout="fixed"
+            className="border-solid object-cover  rounded-full  border-8 border-gray-800"
+          />
+          <h1 className="text-center text-lg mt-5 text-yellow-700">
+            {data[people].name}
+          </h1>
+          <h4 className="text-center ">{data[people].position}</h4>
+          <p className="text-center mt-5">{data[people].description}</p>
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 512 512"
+            className="icon text-yellow-700 text-4xl mt-5"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <Image
-              src={data[people].image}
-              alt="pic"
-              width={150}
-              height={150}
-              layout="fixed"
-              className="border-solid object-cover  rounded-full  border-8 border-gray-800"
-            />
-            <h1 className="text-center text-lg mt-5 text-yellow-700">
-              {data[people].name}
-            </h1>
-            <h4 className="text-center ">{data[people].position}</h4>
-            <p className="text-center mt-5">
-              {data[people].description}
-            </p>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 512 512"
-              className="icon text-yellow-700 text-4xl mt-5"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path>
-            </svg>
-          </div>
+            <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path>
+          </svg>
+        </div>
         <ChevronRightIcon className="h-8 cursor-pointer bg-gray-400 rounded-md ml-3 self-center mt-16" />
       </div>
     </div>
