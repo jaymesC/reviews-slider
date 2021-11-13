75339de31490db0dfa4a52fdd7e1cbadf1f36fee
jaymesC <clementjaymes@gmail.com>
2021-11-13 08:28:49 -0800

    updated project files

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,14 @@ export default function Home() {
         <h1 className="text-center text-gray-700">Reviews</h1>
       </div>
       <div className="flex justify-center mt-16 ">
-        <ChevronLeftIcon className="h-8 cursor-pointer bg-gray-400 rounded-md mr-3 self-center mt-16" />
+        <ChevronLeftIcon
+          className="h-8 cursor-pointer bg-gray-400 rounded-md mr-3 self-center mt-16"
+          onClick={() =>
+            people >= data.length - 1
+              ? setPeople(people - 3)
+              : setPeople(people + 1)
+          }
+        />
         <div
           key={data[people].id}
           className="flex flex-col justify-center items-center text-gray-500 w-1/2"
@@ -49,7 +56,14 @@ export default function Home() {
             <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path>
           </svg>
         </div>
-        <ChevronRightIcon className="h-8 cursor-pointer bg-gray-400 rounded-md ml-3 self-center mt-16" onClick={() => people >= data.length - 1 ? setPeople(people - 3) : setPeople(people + 1)}/>
+        <ChevronRightIcon
+          className="h-8 cursor-pointer bg-gray-400 rounded-md ml-3 self-center mt-16"
+          onClick={() =>
+            people <= data.length - 4
+              ? setPeople(people + 3)
+              : setPeople(people - 1)
+          }
+        />
       </div>
     </div>
   );
